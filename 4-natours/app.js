7b9248aaa014bb974659babb5d7cb95760778408
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-10-20 23:21:51 -0400

    made simple server with express

---
+++ 4-natours/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('hello from the server')
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('app running on port 3000')
+})
