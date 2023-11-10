d203197a26f71114c652042dd61c99b6eac5dc41
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-11-10 17:46:44 -0500

    added new route and controller folders and a server.js for better file structure

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,75 +1,34 @@
-const express = require('express');
+
+const express=require('express');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
-const fs = require('fs');
-app.use(express.json())
+
+app.use(express.json());
 //middleware that is added onto the req object
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/starter/dev-data/data/tours-simple.json`));
-//stores json data into tours variable 
-
-app.get('/api/v1/tours/:id', (req, res) => {
-    console.log(req.params);
-
-    const id = req.params.id *1;
-
-    if (id > tours.length) {
-        return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid ID'
-        });
-    }
-    
-    const tour = tours.find(el => el.id === id);
-
-    res.status(200).json({
-        status: 'success',
-        data:{tour}
-    })
-});
-
-app.post('/api/v1/tours', (req, res) => {
-
-  const newId = tours[tours.length -1].id +1;
-  //creates the number for the newID
-  const newTour = Object.assign({id: newId} , req.body);
-  //Makes a object that contains the id property and whatever is in the req.body object
-
-  tours.push(newTour);
-  //adds the newTour object into our tours array
-
-  fs.writeFile(`${__dirname}/starter/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-        status: 'success',
-        data: {
-            tour: newTour
-        }
-        //we then write the JSON text into the tours-simple.json file. 
-        //we have the json object which is then wrapped around status and the data object
-    })
-  })
-})
-
-app.patch('/api/v1/tours/:id', (req, res) => {
-
-    if (req.params.id * 1 > tours.length) {
-    return res.status (404).json ({
-    status: 'fail',
-    message: 'Invalid ID'
-    });
-    } 
-
-    res.status (200).json({
-        
-    status: 'success',
-    data: {
-    tour: '<Updated tour here...>'
-    }
-
-    });
+app.use((req, res, next) => {
+    console.log('Hello from the middleware');
+    next();
     });
 
+    app.use((req, res, next) => {
+        req.requestTime = new Date().toISOString();
+        next(); 
+    });
 
-const port = 3000;
-app.listen(port, () => {
-    console.log('app running on port 3000')
-})
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
+
+// app.get('/api/v1/tours',getAllTours);
+// app.get('/api/v1/tours/:id',getTour);
+// app.post('/api/v1/tours',createTour)
+// app.patch('/api/v1/tours/:id',updateTour);
+// app.delete('/api/v1/tours/:id',deleteTour);
+
+// const tourRouter = express.Router();
+// const userRouter = express.Router();
---
+++ 4-natours/controllers/tourController.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/../starter/dev-data/data/tours-simple.json`));
+//stores json data into tours variable 
+
+exports.getAllTours = (req, res) => {
+    res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+    tours
+    }
+   });
+  }; 
+
+exports.getTour =  (req, res) => {
+    console.log(req.params);
+
+    const id = req.params.id *1;
+
+    if (id > tours.length) {
+        return res.status(404).json({
+        status: 'fail',
+        message: 'Invalid ID'
+        });
+    }
+    
+    const tour = tours.find(el => el.id === id);
+
+    res.status(200).json({
+        status: 'success',
+        data:{tour}
+    })
+}
+
+exports.createTour =  (req, res) => {
+
+    const newId = tours[tours.length -1].id +1;
+    //creates the number for the newID
+    const newTour = Object.assign({id: newId} , req.body);
+    //Makes a object that contains the id property and whatever is in the req.body object
+  
+    tours.push(newTour);
+    //adds the newTour object into our tours array
+  
+    fs.writeFile(`${__dirname}/starter/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+      res.status(201).json({
+          status: 'success',
+          data: {
+              tour: newTour
+          }
+          //we then write the JSON text into the tours-simple.json file. 
+          //we have the json object which is then wrapped around status and the data object
+      })
+    })
+  }
+
+  exports.updateTour =  (req, res) => {
+
+    if (req.params.id * 1 > tours.length) {
+    return res.status (404).json ({
+    status: 'fail',
+    message: 'Invalid ID'
+    });
+    } 
+
+    res.status (200).json({
+
+    status: 'success',
+    data: {
+    tour: '<Updated tour here...>'
+    }
+
+    });
+    }
+
+    exports.deleteTour =  (req, res) => {
+
+        if (req.params.id * 1 > tours.length) {
+        return res.status (404).json ({
+        status: 'fail',
+        message: 'Invalid ID'
+        });
+        } 
+    
+        res.status(204).json({
+            
+        status: 'success',
+        data:null
+    
+        });
+        }
