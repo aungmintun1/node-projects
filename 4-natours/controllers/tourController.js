e1eeb03f14aab99163a005f713c34283f250c297
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2023-11-17 23:38:15 -0500

    added new create function in tourController

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,83 +1,52 @@
-const fs = require('fs');
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../starter/dev-data/data/tours-simple.json`));
-//stores json data into tours variable 
-
-
-exports.checkID = (req, res, next, val) => {
-    console.log(`checkID is running. Tour id is: ${val}`); 
-    if (req.params.id * 1 > tours.length) {
-    return res.status (404).json ({
-    status: 'fail',
-    message: 'Invalid ID'
-    });
-    }
-    next();
-};
-
-exports.checkBody = (req, res, next,) => {
-    if(!req.body.name || !req.body.price){
-        return res.status(400).json({
-            status: 'fail',
-            message:'Missing name or price'
-        })
-    }
-    next();
-}
+const Tour = require('./../models/tourModel');
 
 exports.getAllTours = (req, res) => {
     res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-    tours
-    }
+    //  
    });
   }; 
 
 exports.getTour =  (req, res) => {
     console.log(req.params);
 
-    const id = req.params.id *1;    
-    const tour = tours.find(el => el.id === id);
+    const id = req.params.id *1;  
 
-    res.status(200).json({
-        status: 'success',
-        data:{tour}
-    })
+    // const tour = tours.find(el => el.id === id);
+
+    // res.status(200).json({
+    //     status: 'success',
+    //     data:{tour}
+    // })
+};
+
+exports.createTour = async (req, res) => {
+ try {
+ // const new Tour = new Tour({})
+ // newTour.save()
+ const newTour = await Tour.create(req.body);
+ res.status (201).json({
+ status: 'success',
+ data: {
+ tour: newTour
+ }
+ });
+
+ } catch (err) {
+ res.status (400).json ({
+ status: 'fail',
+ message: err
+ })
+  };
 }
 
-exports.createTour =  (req, res) => {
-
-    const newId = tours[tours.length -1].id +1;
-    //creates the number for the newID
-    const newTour = Object.assign({id: newId} , req.body);
-    //Makes a object that contains the id property and whatever is in the req.body object
-  
-    tours.push(newTour);
-    //adds the newTour object into our tours array
-  
-    fs.writeFile(`${__dirname}/starter/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-      res.status(201).json({
-          status: 'success',
-          data: {
-              tour: newTour
-          }
-          //we then write the JSON text into the tours-simple.json file. 
-          //we have the json object which is then wrapped around status and the data object
-      })
-    })
-  }
-
   exports.updateTour =  (req, res) => {
-
-    res.status (200).json({
-
+    res.status(200).json({
     status: 'success',
     data: {
     tour: '<Updated tour here...>'
     }
-
     });
     }
 
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,10 +1,9 @@
-
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-/*
+
 
 const DB = process.env.DATABASE.replace('<PASSWORD>' , process.env.DATABASE_PASSWORD);
 
@@ -15,41 +14,9 @@ mongoose.connect(DB, {
 }).then(() => {console.log('DB connection successful')
 })
 
-const tourSchema = new mongoose.Schema ({
-    name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-    },
-
-    rating: {
-    type: Number,
-    default: 4.5
-    },
-
-    price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-    }
-    });
-
-    const Tour = mongoose.model('Tour', tourSchema);
-
-    const testTour = new Tour({
-        name: 'johnathan',
-        rating: 4.7,
-        price: 497
-        });
-
-        testTour.save().then(doc => {
-        console.log(doc);
-        }).catch(err => {
-        console.log('ERROR err')
-        })
-        */
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('app running on port 3000')
+    console.log('running on port')
+    console.log(port)
    
 })
