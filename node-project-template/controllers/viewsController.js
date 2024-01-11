68a0a4282e03e14b132cae7fb435cabb28648d5a
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2024-01-11 12:53:59 -0500

    added pug modules and basic template into project

---
+++ node-project-template/controllers/viewsController.js
@@ -0,0 +1,16 @@
+// const Shirt = require('./../models/shirtModel');
+// const catchAsync = require('./../utils/catchAsync');
+// const AppError = require('./../utils/appError')
+
+// exports.getOverview = catchAsync(async (req, res, next) => {
+//     // 1) Get tour data from collection
+//     const shirts = await Shirt.find();
+  
+//     // 2) Build template
+//     // 3) Render that template using tour data from 1)
+//     res.status(200).render('overview', {
+//       title: 'All Tours',
+//       shirts
+//     });
+//   });
+  
---
+++ node-project-template/routes/viewRoutes.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const viewsController = require('./../controllers/viewsController');
+// const router = express.Router();
+
+// // router
+// // .route('/')
+// // .get(shirtController.getAllShirt)
+
+// module.exports = router;
