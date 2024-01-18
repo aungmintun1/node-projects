da7b413d9ea2b43e548e2fb6cf16d74b75ab8d86
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2024-01-18 13:04:39 -0500

    added referene modeling to users

--- node-project-template/controllers/userController.js
+++ node-project-template/controllers/userController.js
@@ -17,9 +17,9 @@ exports.getAllUsers = catchAsync(async(req,res,next) => {
 exports.createUser =  catchAsync(async (req,res,next) => {
      const newUser = await User.create(req.body);
 
-     const url = `${req.protocol}://${req.get('host')}/getUser`;
-     console.log(url);
-     await new Email(newUser, url).sendWelcome();
+    //  const url = `${req.protocol}://${req.get('host')}/getUser`;
+    //  console.log(url);
+    //  await new Email(newUser, url).sendWelcome();
      
      res.status(201).json({
        status: 'success',
@@ -31,7 +31,9 @@ exports.createUser =  catchAsync(async (req,res,next) => {
 });
 
 exports.getUser = catchAsync(async (req, res, next) => {
-    let doc = await User.findById(req.params.id); 
+    let doc = await User.findById(req.params.id).populate({
+      path:'shirts'
+    })
 
     res.status(200).json({
       status: 'success',
@@ -40,3 +42,33 @@ exports.getUser = catchAsync(async (req, res, next) => {
       }
     });
   });
+
+  exports.updateUser = catchAsync(async(req,res,next) => {
+    
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+
+});
+  
