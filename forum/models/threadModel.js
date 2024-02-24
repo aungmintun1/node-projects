6ae196adcc87c45b4e938213c3fef350a7582902
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2024-02-24 12:01:51 -0500

    added login page and new css styling

--- forum/models/threadModel.js
+++ forum/models/threadModel.js
@@ -8,17 +8,28 @@ question: {
 },
 
 },
+
+// total thread likes
+// total # of comments
+
 {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
-}
-)
+});
 
 threadSchema.virtual('comments', {
     ref: 'Comment',
     foreignField: 'thread',
     localField: '_id'
   });
+
+  threadSchema.virtual('likes', {
+    ref: 'Like',
+    foreignField: 'thread',
+    localField: '_id'
+  });
+
+//total likes method
 
 const Thread = mongoose.model('Thread', threadSchema);
 module.exports = Thread;
